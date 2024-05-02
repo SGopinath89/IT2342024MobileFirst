701fc50854ce3b2fc7683c158b96b99ff5c6e958
Nazik <shmnazik79@gmail.com>
2024-05-02 22:05:45 +0530

    Created pages and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './Pages/Home';
+import Products from './Pages/Products';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
 
 function App() {
   return (
     <div>
-      <NavBar/>
+      <BrowserRouter>
+        <NavBar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/products' element={<Products/>}/>
+          <Route path='/product' element={<Product/>}>
+            <Route path=':productId' element={<Product/>}/>
+          </Route>
+          <Route path='/cart' element={<Cart/>}/>
+        </Routes>      
+      </BrowserRouter>
     </div>
   );
 }
